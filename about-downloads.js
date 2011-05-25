e4b269ecf010297de625144afe0eabd69e030c10
Irakli Gozalishvili <rfobic@gmail.com>
2011-05-25 12:25:57 +0200

    Adding super ugly implementation of: http://limi.net/articles/improving-download-behaviors-web-browsers

--- about-downloads.js
+++ about-downloads.js
@@ -3,6 +3,8 @@
 'use strict'
 
 const { Handler } = require("protocol");
+const { Panel } = require("panel");
+const { Widget } = require("widget");
 
 exports.handler = Handler({
   onRequest: function onRequest(request, response) {
@@ -10,4 +12,11 @@ exports.handler = Handler({
   }
 });
 
+exports.panel = Panel({ contentURL: "about:downloads" });
+exports.widget = Widget({
+  id: "about-downloads-button",
+  label: "Dowloads",
+  contentURL: "chrome://browser/skin/places/searching_16.png",
+  panel: exports.panel
+});
 exports.handler.listen({ about: "downloads" })
